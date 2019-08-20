c64cd0be6fc97359766c0f4588c47f938cbbf53d
caleighlester <ubuntu@ide.ide.78b549c3-80f4-41d3-9010-54cc5cdfe584.svc.cluster.local>
2019-08-20 14:14:48 +0000

    Copy over files from Monday

---
+++ HelloName.java
@@ -0,0 +1,9 @@
+public class HelloName
+{
+    public static void main(String[] args)
+    {
+        String name = "CaleighB";
+        System.out.println("Hello, " + name);
+
+    }
+}
---
+++ TalkingToMe.java
@@ -0,0 +1,15 @@
+public class TalkingToMe
+{
+    public static void main(String[] args);
+    {
+        if (args.length !=2);
+        {
+            System.out.println("Usage: java HelloInput firstName lastName")
+        }
+        else
+        {
+            String firstName =
+        }
+
+    }
+}
