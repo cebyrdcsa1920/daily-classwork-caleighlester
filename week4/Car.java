05097219bfff7f59a42e8ee49b1505af19af6856
caleighlester <ubuntu@ide.ide.78b549c3-80f4-41d3-9010-54cc5cdfe584.svc.cluster.local>
2019-09-04 13:47:36 +0000

    Create Car.java and its no-args constructor

---
+++ week4/Car.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+
+public class Car
+{
+    private String model;
+    private Color color;
+    private int numPassangers;
+    private double amountOfGas;
+
+
+    public Car()
+    {
+        model = "Camaro";
+        color = Color.YELLOW;
+        numPassenger = 2;
+        amountOfGas = 12.5;
+    }
+
+}
